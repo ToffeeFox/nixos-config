f7bbee96e8d01e4148e84d7df8ee93e5cc1724cf
ToffeeFox <dev@toffeefox.com>
2026-09-11 14:35:17 -0500

    Corrected typo in fido2 option name

--- boot.nix
+++ boot.nix
@@ -48,7 +48,7 @@ in
       luks = {
         # Counterintuitively, we need to disable FIDO2 support to use
         # FIDO2 with systemd stage 1.
-        fido2support = false;
+        fido2Support = false;
 
         devices = {
           "luks-${disks.rootUUID}" = {
