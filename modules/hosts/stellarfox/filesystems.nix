cd173fc0a7eaa5626129b76a51dfd6b4bdbf215a
ToffeeFox <dev@toffeefox.com>
2026-09-15 13:10:13 -0500

    Started host-specific modular config for stellarfox

---
+++ modules/hosts/stellarfox/filesystems.nix
@@ -0,0 +1,62 @@
+let
+  disks = {
+    rootUUID = "983b1082-724b-4108-8226-f70d098dddcf";
+    swapUUID = "5c9b5633-7943-4643-b7ca-769ce43444cd";
+  };
+in
+{
+  den.aspects.stellarfox.nixos = {
+    filesystems."/" = {
+      device = "/dev/mapper/luks-${disks.rootUUID}";
+      fsType = "btrfs";
+    };
+
+    boot.initrd.luks.devices = {
+      "luks-${disks.rootUUID}" = {
+        crypttabExtraOpts = [
+          "fido2-device=auto"
+        ];
+        device = "/dev/disk/by-uuid/${disks.rootUUID}";
+      };
+      /*
+      "luks-${disks.swapUUID}" = {
+        crypttabExtraOpts = [
+          "fido2-device=auto"
+        ];
+        device = "/dev/disk/by-uuid/${disks.swapUUID}";
+      };
+      */
+    };
+
+    filesystems."/nix" = {
+      device = "/dev/mapper/luks-${disks.rootUUID}";
+      fsType = "btrfs";
+      options = [
+        "subvol=nix"
+      ];
+    };
+
+    filesystems."/home" = {
+      device = "/dev/mapper/luks-${disks.rootUUID}";
+      fsType = "btrfs";
+      options = [
+        "subvol=home"
+      ];
+    };
+
+    fileSystems."/boot" = {
+      device = "/dev/disk/by-uuid/B288-F330";
+      fsType = "vfat";
+      options = [
+        "fmask=0077"
+        "dmask=0077"
+      ];
+    };
+
+    swapDevices = [
+      {
+        device = "/dev/mapper/luks-${disks.swapUUID}";
+      }
+    ];
+  };
+}
