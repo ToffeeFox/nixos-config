8c15852924b546b8f8cb5bed15954123d0ac42c7
ToffeeFox <dev@toffeefox.com>
2026-09-17 14:55:29 -0500

    created generic boot.nix (currently named denboot.nix)

---
+++ modules/denboot.nix
@@ -0,0 +1,43 @@
+{ lib, ... }:
+{
+  flux.boot.provides = {
+    secure.nixos = {
+      boot = {
+        loader = {
+          systemd-boot.enable = lib.mkForce false;
+
+          limine = {
+            enable = true;
+
+            secureBoot.enable = true;
+
+            style = {
+              interface = {
+                branding = "Lukida's Customized Limine Bootloader";
+              };
+            };
+          };
+        };
+      };
+    };
+
+    graphical.nixos.boot = {
+      plymouth = {
+        enable = true;
+        theme = "bgrt";
+      };
+
+      consoleLogLevel = 3;
+      initrd.verbose = false;
+
+      initrd.systemd.enable = true;
+
+      kernelParams = [
+        "splash"
+        "boot.shell_on_fail"
+        "udev.log_priority=3"
+        "rd.systemd.show_status=auto"
+      ];
+    };
+  };
+}
