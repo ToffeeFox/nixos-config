a39f13b7ab2b1332e5d43edcd0b5779e1eb27b4d
ToffeeFox <dev@toffeefox.com>
2026-09-17 13:14:49 -0500

    pull in den, flake-parts, import-tree, zen-browser, and niri flakes. Also update flake

--- flake.nix
+++ flake.nix
@@ -2,18 +2,38 @@
   description = "NixOS configuration with flakes";
 
   inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    import-tree.url = "github:denful/import-tree";
+    den.url = "github:denful/den";
 
     nixpkgs = {
       url = "https://channels.nixos.org/nixos-unstable/nixexprs.tar.xz";
     };
 
-    flake-parts = {
-      url = "github:hercules-ci/flake-parts";
-    };
-
     nixos-hardware = {
       url = "github:NixOS/nixos-hardware";
       inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    zen-browser = {
+      url = "github:0xc000022070/zen-browser-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
+    };
+
+    niri = {
+      url = "github:epireyn/niri-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+      # using niri from nixpkgs
+      inputs.niri-unstable.follows = "";
+      inputs.niri-stable.follows = "";
+      inputs.xwayland-satellite-unstable.follows = "";
+      inputs.xwayland-satellite-stable.follows = "";
     };
 
     noctalia = {
