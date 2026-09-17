2cba834fe4867def18845e44f40ebe927af908fa
ToffeeFox <dev@toffeefox.com>
2026-09-17 15:18:29 -0500

    Create zen-browser.nix

---
+++ modules/apps/zen-browser.nix
@@ -0,0 +1,8 @@
+{ inputs, ... }:
+{
+  flux.apps._.zen.homeManager = {
+    imports = [ inputs.zen-browser.homeModules.default ];
+    programs.zen-browser.enable = true;
+    home.sessionVariables.BROWSER = "zen-beta";
+  };
+}
