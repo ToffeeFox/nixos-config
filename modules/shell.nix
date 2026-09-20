2a8249f48d83815c1b58b1618dc996392aeeabc8
ToffeeFox <dev@toffeefox.com>
2026-09-20 01:06:03 -0500

    Add shell module with aliases and Atuin configuration

---
+++ modules/shell.nix
@@ -0,0 +1,34 @@
+{
+  flux.shell = {
+    homeManager = { pkgs, ... }: {
+      home.shellAliases = {
+        # Quick Utils
+        ll = "ls -l";
+        la = "ls -A";
+
+        # System Mgmt
+        hm = "home-manager";
+        nixup = "sudo nixos-rebuild switch";
+        homeup = "home-manager switch -b backup";
+        flakeup = "nix flake update";
+        flakecheck = "nix flake check --verbose --show-trace";
+        sysflake-sync = "sudo rm -r /etc/nixos/* && sudo cp -r ~/nixos-config/* /etc/nixos/";
+      };
+
+      programs.atuin = {
+        enable = true;
+
+        daemon.enable = true;
+
+        enableZshIntegration = true;
+
+        settings = {
+          search_mode = "fuzzy";
+        };
+
+        # In case Atuin decides it is too good for our nix config
+        forceOverwriteSettings = true;
+      };
+    };
+  };
+}
