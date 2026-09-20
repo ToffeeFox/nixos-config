c2315b41310f9f3bf32d4193e819eb6a4b861d05
ToffeeFox <dev@toffeefox.com>
2026-09-20 01:05:52 -0500

    Add saluki module
    Define the saluki aspect and include primary user, flux, and
    application configurations.

---
+++ modules/saluki.nix
@@ -0,0 +1,15 @@
+{ __findFile, ... }:
+{
+  den.aspects.saluki = {
+    includes = [
+      <den/primary_user>
+      <flux/niri>
+      <flux/xdg>
+      <flux/zsh>
+
+      <flux/apps/patched_logseq>
+      <flux/apps/zed-editor>
+      <flux/apps/zen>
+    ];
+  };
+}
