3cce09b02ce5f4affff29c6e9b365176886cd2e8
ToffeeFox <dev@toffeefox.com>
2026-09-20 01:06:35 -0500

    Add core module structure and stellarfox host configuration

---
+++ modules/den.nix
@@ -0,0 +1,10 @@
+{ inputs, den, ... }:
+{
+  _module.args.__findFile = den.lib.__findFile;
+  den.schema.user.includes = [ den._.mutual-provider ];
+  flake.den = den;
+  imports = [
+    inputs.den.flakeModule
+    (inputs.den.namespace "flux" true)
+  ];
+}
---
+++ modules/hosts/stellarfox/default.nix
@@ -0,0 +1,38 @@
+{ flux, inputs, ... }:
+{
+  den.hosts.x86_64-linux.stellarfox = {
+    users.saluki.classes = [ "homeManager" ];
+    displays = {
+      eDP-1 = {
+        refresh = 60.0;
+        width = 1920;
+        height = 1200;
+        scaling = 1.20;
+        wallpaper = /assets/background.png;
+      };
+    };
+  };
+  den.aspects.saluki = {
+    includes = with flux; [
+
+    ];
+
+    nixos =
+      { pkgs, ... }:
+      {
+        imports = [ inputs.nixos-hardware.nixosModules.dell-precision-5570 ];
+
+        boot = {
+          plymouth.enable = true;
+        };
+
+        networking.hostName = "stellarfox";
+        networking.networkmanager.enable = true;
+
+        services = {
+          fprintd.enable = true; # Enable fingerprint scanner
+          fwupd.enable = true; # Enable firmware updates with `fwupdmgr update`
+        };
+      };
+  };
+}
