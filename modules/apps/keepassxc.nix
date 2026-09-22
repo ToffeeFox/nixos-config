f06c9f6c0232b12e6d1d35e10879ea54942823a4
ToffeeFox <dev@toffeefox.com>
2026-09-22 16:07:51 -0500

    Refactor KeePassXC Home Manager module interface

--- modules/apps/keepassxc.nix
+++ modules/apps/keepassxc.nix
@@ -1,55 +1,57 @@
-{ lib, ... }:
-
 {
-  flux.apps._.keepassxc.homeManager = {
-    programs.keepassxc = {
-      enable = true;
-      autostart = true;
+  flux.apps._.keepassxc = {
+    homeManager =
+    { lib, ... }:
+    {
+      programs.keepassxc = {
+        enable = true;
+        autostart = true;
 
-      settings = {
-        # Prevent error messages related to browser integration installation failure
-        # programs.keepass.xc.enable handles browser integration installs automatically
-        Browser.UpdateBinaryPath = false;
+        settings = {
+          # Prevent error messages related to browser integration installation failure
+          # programs.keepass.xc.enable handles browser integration installs automatically
+          Browser.UpdateBinaryPath = false;
 
-        GUI = {
-          AdvancedSettings = true;
-          ApplicationTheme = "dark";
+          GUI = {
+            AdvancedSettings = true;
+            ApplicationTheme = "dark";
 
-          ColorPasswords = lib.mkDefault true;
+            ColorPasswords = lib.mkDefault true;
 
-          ShowTrayIcon = true;
-        };
+            ShowTrayIcon = true;
+          };
 
-        PasswordGenerator = {
-          WordSeparator = "-";
-        };
+          PasswordGenerator = {
+            WordSeparator = "-";
+          };
 
-        Security = {
-          ClearClipboard = lib.mkForce true;
-          ClearClipboardTimeout = lib.mkForce 10;
+          Security = {
+            ClearClipboard = lib.mkForce true;
+            ClearClipboardTimeout = lib.mkForce 10;
 
-          LockDatabaseIdle = lib.mkForce true;
-          LockDatabaseIdleSeconds = lib.mkForce 900;
+            LockDatabaseIdle = lib.mkForce true;
+            LockDatabaseIdleSeconds = lib.mkForce 900;
 
-          LockDatabaseMinimize = false; # I'm chill with this one being fucked with tbh
-          LockDatabaseScreenLock = lib.mkForce true;
-          LockDatabaseOnUserSwitch = lib.mkForce true;
-        };
+            LockDatabaseMinimize = false; # I'm chill with this one being fucked with tbh
+            LockDatabaseScreenLock = lib.mkForce true;
+            LockDatabaseOnUserSwitch = lib.mkForce true;
+          };
 
-        # Use KeePassXC for Secret Service
-        FdoSecrets = {
-          Enabled = true;
+          # Use KeePassXC for Secret Service
+          FdoSecrets = {
+            Enabled = true;
 
-          ShowNotification = lib.mkForce true;
+            ShowNotification = lib.mkForce true;
 
-          ConfirmAccessItem = lib.mkForce true;
-          ConfirmDeleteItem = lib.mkForce true;
+            ConfirmAccessItem = lib.mkForce true;
+            ConfirmDeleteItem = lib.mkForce true;
 
-          UnlockBeforeSearch = lib.mkForce true;
+            UnlockBeforeSearch = lib.mkForce true;
+          };
         };
       };
-    };
 
-    xdg.autostart.enable = true;
+      xdg.autostart.enable = true;
+    };
   };
 }
