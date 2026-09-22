91cf85fd4e610be780035cba0062b127fa0272c1
ToffeeFox <dev@toffeefox.com>
2026-09-22 11:50:27 -0500

    Create brave.nix

---
+++ modules/apps/browsers/brave.nix
@@ -0,0 +1,15 @@
+{
+  flux.browser.brave = {
+    homeManager = {
+      programs.brave = {
+        enable = true;
+
+        homepageLocation = "https://www.startpage.com/";
+
+        commandLineArgs = [
+          ""
+        ];
+      };
+    };
+  };
+}
