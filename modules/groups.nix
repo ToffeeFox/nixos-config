61e4a61e890523054038241c056fec39efd4eb20
ToffeeFox <dev@toffeefox.com>
2026-09-23 13:05:56 -0500

    Create groups.nix

---
+++ modules/groups.nix
@@ -0,0 +1,16 @@
+{ flux, den, ... }:
+{
+  flux.batteries._.privileged-user =
+    { user, ... }:
+    {
+      nixos =
+        { lib, config, ... }:
+        {
+          config.users.users.${user.userName}.extraGroups = config.users.privilegedGroups;
+          options.users.privilegedGroups = lib.mkOption {
+            type = lib.types.listOf lib.types.str;
+            default = [ ];
+          };
+        };
+    };
+}
