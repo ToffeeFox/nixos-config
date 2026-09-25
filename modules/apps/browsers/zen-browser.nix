58317b52333636ac0a8a5e8016e227260f0e009a
ToffeeFox <dev@toffeefox.com>
2026-09-25 15:50:26 -0500

    chore: code formatting in zen-browser.nix

--- modules/apps/browsers/zen-browser.nix
+++ modules/apps/browsers/zen-browser.nix
@@ -1,8 +1,12 @@
 { inputs, ... }:
 {
-  flux.apps.browser.zen.homeManager = {
-    imports = [ inputs.zen-browser.homeModules.default ];
-    programs.zen-browser.enable = true;
-    home.sessionVariables.BROWSER = "zen-beta";
+  flux.apps.browser.zen = {
+    homeManager = { config, ... }: {
+      imports = [ inputs.zen-browser.homeModules.default ];
+
+      programs.zen-browser.enable = true;
+
+      home.sessionVariables.BROWSER = "zen-beta";
+    };
   };
 }
